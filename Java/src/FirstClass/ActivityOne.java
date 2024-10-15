422d65f9df84f955aa9653dc8d82e42c887cad20
R2d2cruz <r2d2cruz@gmail.com>
2024-10-15 17:53:01 -0500

    Add last part of activity one

--- Java/src/FirstClass/ActivityOne.java
+++ Java/src/FirstClass/ActivityOne.java
@@ -3,5 +3,7 @@ package FirstClass;
 public class ActivityOne {
     public static void printHelloWorld() {
         System.out.println("Hola mundo!");
+        System.out.println("Mi nombre es Carlos Arturo Cruz");
+        System.out.println("Mi edad es 22 años");
     }
 }
