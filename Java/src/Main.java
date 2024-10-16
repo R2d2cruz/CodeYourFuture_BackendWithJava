2331427f9245802722b40a8203094a559e3f9988
R2d2cruz <r2d2cruz@gmail.com>
2024-10-16 14:14:07 -0500

    Create the main menu
    In the class main was added:
    - main: principal loop of excecution
    - menu: Print the menu off options
    - inputInt: It ask for the option input
    - clearTerminal: clear the terminal
    - waitEnter: wait for the user to put enter

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,5 +1,60 @@
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        boolean inLoop = true;
+        do {
+            menu();
+            switch (inputInt(1, "Ingrese la opcion que prefiere: ")) {
+                case 0 -> {
+                    System.out.println("Muchas gracias, que tenga buen dia.");
+                    inLoop = false;
+                }
+                case 1 -> {
+                }
+                default -> throw new AssertionError();
+            }
+        } while (inLoop);
+        scanner.close();
+    }
+
+    public static void menu() {
+        clearTerminal();
+        System.out.println("Menu de clases, seleccione una: ");
+        System.out.println("1. Clase Uno");
+        System.out.println("0. Salir");
+    }
+
+    public static int inputInt(int maxValue, String message) {
+        return inputInt(maxValue, message, "Numero no valido, ingrese nuevamente");
+    }
+
+    public static int inputInt(int maxValue, String message, String errorMessage) {
+        int val = 0;
+        boolean isNotvalid;
+        do {
+            System.out.print(message);
+            val = scanner.nextInt();
+            isNotvalid = val < 0 || val > maxValue;
+            if (isNotvalid) {
+                System.out.println(errorMessage);
+            }
+        } while (isNotvalid);
+        return val;
+    }
+
+    public static void clearTerminal(){
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();
+    }
+
+    public static void waitEnter(){
+        scanner.nextLine();
+        System.out.println("Presione Enter para continuar...");
+        scanner.nextLine();
+    }
+
     }
 }
