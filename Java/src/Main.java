2a461be266db706426cd930d479415a5952d02d3
R2d2cruz <r2d2cruz@gmail.com>
2024-10-28 15:19:43 -0500

    add and create class four activity three

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -124,6 +124,7 @@ public class Main {
             switch (option) {
                 case 1 -> ClassFour.ActivityOne.determineNumber();
                 case 2 -> ClassFour.ActivityTwo.rating();
+                case 3 -> ClassFour.ActivityThree.divisible35();
                 default -> throw new AssertionError("How did we get here?");
             }
             System.out.println("Funcion finalizada, volviendo al menu...");
