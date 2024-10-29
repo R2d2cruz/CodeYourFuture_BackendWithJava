05fc5a5992af48b42bbe176f0fdaf8572fee6637
R2d2cruz <r2d2cruz@gmail.com>
2024-10-29 17:35:16 -0500

    add and create Activity one

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -288,6 +288,7 @@ public class Main {
         int option = menuActivities(4, 0,"quince");
         if (option != 0) {
             switch (option) {
+                case 1 -> ClassFifteen.ActivityOne.passwordGenerator();
                 default -> throw new AssertionError("How did we get here?");
             }
             System.out.println("Funcion finalizada, volviendo al menu...");
