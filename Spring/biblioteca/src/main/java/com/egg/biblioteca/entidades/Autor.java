4a9bd868397ccacf9be308da7280a395628fcc4e
R2d2cruz <r2d2cruz@gmail.com>
2025-02-26 17:37:50 -0500

    create entities

---
+++ Spring/biblioteca/src/main/java/com/egg/biblioteca/entidades/Autor.java
@@ -0,0 +1,43 @@
+package com.egg.biblioteca.entidades;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "autor")
+public class Autor {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    public Autor() {
+    };
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+}
