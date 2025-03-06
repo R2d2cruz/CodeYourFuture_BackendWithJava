38b53f24cb34960029115801e72a547dc4e27442
R2d2cruz <r2d2cruz@gmail.com>
2025-03-05 20:06:48 -0500

    add error handler in EditorialControlador

--- Spring/biblioteca/src/main/java/com/egg/biblioteca/controladores/EditorialControlador.java
+++ Spring/biblioteca/src/main/java/com/egg/biblioteca/controladores/EditorialControlador.java
@@ -5,11 +5,13 @@ import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.egg.biblioteca.entidades.Editorial;
 import com.egg.biblioteca.excepciones.MiException;
 import com.egg.biblioteca.servicios.EditorialServicio;
 
@@ -26,11 +28,13 @@ public class EditorialControlador {
     }
 
     @PostMapping("/registro")
-    public String registro(@RequestParam String nombre) {
+    public String registro(@RequestParam String nombre, ModelMap modelo) {
         try {
             editorialServicio.crearEditorial(nombre); // llamo a mi servicio para persistir
+            modelo.put("exito", "La editorial se guardo correctamente");
         } catch (MiException ex) {
             Logger.getLogger(EditorialControlador.class.getName()).log(Level.SEVERE, null, ex);
+            modelo.put("error", ex.getMessage());
             return "editorial_form.html";
         }
         return "index.html";
