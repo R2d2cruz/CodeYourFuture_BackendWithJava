b3eb0df22ef54ff39b81e6503a38190886fe6da2
R2d2cruz <r2d2cruz@gmail.com>
2025-03-10 17:25:04 -0500

    creation of users, rols and security input using spring

--- Spring/biblioteca/src/main/java/com/egg/biblioteca/controladores/PortalControlador.java
+++ Spring/biblioteca/src/main/java/com/egg/biblioteca/controladores/PortalControlador.java
@@ -1,16 +1,55 @@
 package com.egg.biblioteca.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.egg.biblioteca.excepciones.MiException;
+import com.egg.biblioteca.servicios.UsuarioServicio;
 
 @Controller
 @RequestMapping("/")
 public class PortalControlador {
 
-    @GetMapping("/")  // Acá es donde realizamos el mapeo
+    @Autowired
+    private UsuarioServicio usuarioServicio;
+
+    @GetMapping("/") // Acá es donde realizamos el mapeo
     public String index() {
-        return "index.html";   // Acá es que retornamos con el método. 
+        return "index.html"; // Acá es que retornamos con el método.
     }
- 
+
+    @GetMapping("/registrar")
+    public String registrar() {
+        return "registro.html";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login.html";
+    }
+
+    @PostMapping("/registro")
+    public String registro(@RequestParam("nombre") String nombre,
+            @RequestParam("email") String email,
+            @RequestParam("password") String password,
+            @RequestParam("password2") String password2,
+            ModelMap modelo) {
+        try {
+            // Intentar registrar al usuario utilizando el servicio
+            usuarioServicio.registrarUsuario(nombre, email, password, password2);
+
+            // Si todo es correcto, redirigir a la vista index.html y pasar mensaje de éxito
+            modelo.put("exito", "Tu usuario ha sido registrado con exito!");
+            return "index.html"; // Nombre de la vista
+        } catch (MiException ex) {
+            // Si hay un error, devolver al formulario con un mensaje de error
+            modelo.put("error", ex.getMessage());
+            return "registro.html"; // Nombre del formulario de registro
+        }
+    }
 }
---
+++ Spring/biblioteca/src/main/java/com/egg/biblioteca/entidades/Usuario.java
@@ -0,0 +1,84 @@
+package com.egg.biblioteca.entidades;
+
+import java.util.UUID;
+
+import com.egg.biblioteca.enumeraciones.Rol;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "rol")
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
+
+    public Usuario() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+
+
+
+    
+}
