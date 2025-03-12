667e138c0e80f31c33154b7b620d890fa5b9f254
R2d2cruz <r2d2cruz@gmail.com>
2025-03-12 17:14:21 -0500

    Implementation of HTTPSession, security restrictions, and fragments.

--- Spring/biblioteca/src/main/java/com/egg/biblioteca/SeguridadWeb.java
+++ Spring/biblioteca/src/main/java/com/egg/biblioteca/SeguridadWeb.java
@@ -11,18 +11,38 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SeguridadWeb {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/css/", "/js/", "/img/", "/**").permitAll())
-                .csrf(csrf -> csrf.disable());
-        return http.build();
-    }
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http
+                                .authorizeHttpRequests((authorize) -> authorize
+                                                .requestMatchers("/admin/**", "/libro/registrar", "/libro/modificar/**",
+                                                                "/autor/registrar", "/autor/modificar/**",
+                                                                "/editorial/registrar", "/editorial/modificar/**")
+                                                .hasRole("ADMIN")
+                                                .requestMatchers("/libro/lista", "/autor/lista", "/editorial/lista")
+                                                .hasAnyRole("ADMIN", "USER")
+                                                .requestMatchers("/css/**", "/js/", "/img/**").permitAll()
+                                                .requestMatchers("/", "/login", "/registrar").permitAll() // Rutas accesibles
+                                                                                                     // sin login
+                                                .anyRequest().authenticated())
+                                .formLogin((form) -> form
+                                                .loginPage("/login")
+                                                .loginProcessingUrl("/logincheck")
+                                                .usernameParameter("email")
+                                                .passwordParameter("password")
+                                                .defaultSuccessUrl("/inicio", true)
+                                                .permitAll())
+                                .logout((logout) -> logout
+                                                .logoutUrl("/logout")
+                                                .logoutSuccessUrl("/login")
+                                                .permitAll())
+                                .csrf(csrf -> csrf.disable());
+                return http.build();
+        }
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        public BCryptPasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
 }
--- Spring/biblioteca/src/main/java/com/egg/biblioteca/servicios/UsuarioServicio.java
+++ Spring/biblioteca/src/main/java/com/egg/biblioteca/servicios/UsuarioServicio.java
@@ -13,11 +13,15 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.egg.biblioteca.entidades.Usuario;
 import com.egg.biblioteca.enumeraciones.Rol;
 import com.egg.biblioteca.excepciones.MiException;
 import com.egg.biblioteca.repositorios.UsuarioRepositorio;
+
+import jakarta.servlet.http.HttpSession;
 
 @Service
 public class UsuarioServicio implements UserDetailsService {
@@ -67,10 +71,17 @@ public class UsuarioServicio implements UserDetailsService {
 
             permisos.add(p);
 
+            ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+
+            HttpSession session = attr.getRequest().getSession(true);
+
+            session.setAttribute("usuariosession", usuario);
+            
             return new User(usuario.getEmail(), usuario.getPassword(), permisos);
         } else {
             return null;
         }
+
     }
 
 }
