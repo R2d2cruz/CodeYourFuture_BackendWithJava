c22f8dddafbfdebea79a1a62e0ee1364ac866884
R2d2cruz <r2d2cruz@gmail.com>
2025-03-17 18:44:56 -0500

    creation the "mercaderia" project

---
+++ Spring/mercaderia/src/main/java/com/egg/mercaderia/MercaderiaApplication.java
@@ -0,0 +1,13 @@
+package com.egg.mercaderia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercaderiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercaderiaApplication.class, args);
+	}
+
+}
---
+++ Spring/mercaderia/src/test/java/com/egg/mercaderia/MercaderiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egg.mercaderia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercaderiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
